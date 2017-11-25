a6ed7e574edce4a3befe2356accab9e79e5f95e9
Bohdan Pashkovskyi <bpashkovskyy@us.imshealth.com>
2017-11-25 13:35:57 +0200

    fix initiakized

--- WebTemplate.Database/ProjectContextInitializer.cs
+++ WebTemplate.Database/ProjectContextInitializer.cs
@@ -13,10 +13,10 @@ namespace WebTemplate.Database
             var category1 = new Category { Name = "Category 1" };
             var category2 = new Category { Name = "Category 2" };
 
-            var news11 = new News { Title = "News 11", Text = "11" };
-            var news12 = new News { Title = "News 12", Text = "11" };
-            var news21 = new News { Title = "News 21", Text = "21" };
-            var news22 = new News { Title = "News 22", Text = "22" };
+            var news11 = new News { Title = "News 11", Text = "11", Tags = "Tag1, Tag2"};
+            var news12 = new News { Title = "News 12", Text = "11", Tags = "Tag1, Tag3" };
+            var news21 = new News { Title = "News 21", Text = "21", Tags = "Tag2, Tag3" };
+            var news22 = new News { Title = "News 22", Text = "22", Tags = "Tag4, Tag3" };
 
             category1.News = new List<News> { news11, news12 };
             category2.News = new List<News> { news21, news22 };
