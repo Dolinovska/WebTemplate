a4fabe147767d947f1bfc13c81624ef904293740
Dmytro Tsiniavskyi <dtsiniavskyi@saasap.net>
2017-11-25 15:19:41 +0200

    fixing parserr, populate database with parsed news

--- WebTemplate.Database/ProjectContextInitializer.cs
+++ WebTemplate.Database/ProjectContextInitializer.cs
@@ -10,20 +10,16 @@ namespace WebTemplate.Database
     {
         protected override void Seed(WebTemplateContext context)
         {
-            var category1 = new Category { Name = "Category 1" };
-            var category2 = new Category { Name = "Category 2" };
+            var category1 = new Category { Name = "All" };
 
             var news11 = new News { Title = "News 11", Text = "11", Tags = "Tag1,Tag2" };
             var news12 = new News { Title = "News 12", Text = "11", Tags = "Tag1,Tag3" };
             var news21 = new News { Title = "News 21", Text = "21", Tags = "Tag2,Tag3" };
             var news22 = new News { Title = "News 22", Text = "22", Tags = "Tag4,Tag3" };
 
-            category1.News = new List<News> { news11, news12 };
-            category2.News = new List<News> { news21, news22 };
-
+            category1.News = new List<News> { news11, news12, news21, news22 };
 
             context.Categories.Add(category1);
-            context.Categories.Add(category2);
             context.SaveChanges();
         }
     }
