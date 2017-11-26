bae408ae284136043a59d2f2e7d7428ddee9df7e
Svitlana Dolinovska <sdolinovska@gmail.com>
2017-11-26 04:39:08 +0200

    Add order

--- WebTemplate.MVC/Controllers/NewsController.cs
+++ WebTemplate.MVC/Controllers/NewsController.cs
@@ -115,6 +115,12 @@ namespace WebTemplate.MVC.Controllers
 
             return View(newsEditModel);
         }
+        public ActionResult Order()
+        {
+            return View();
+        }
+
+       
 
         public ActionResult Edit(int? id)
         {
