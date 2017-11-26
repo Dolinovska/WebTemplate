e704b211e177af20a0aea43aa83be3277a1086b8
Dmytro Tsiniavskyi <dtsiniavskyi@saasap.net>
2017-11-26 05:09:11 +0200

    fix bootstrap-tagsinput

--- WebTemplate.MVC/App_Start/BundleConfig.cs
+++ WebTemplate.MVC/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace WebTemplate.MVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-tagsinput.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
